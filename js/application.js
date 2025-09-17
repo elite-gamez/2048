efa8bba4fac692fd7e3f52f54b2e7f7d13ad0922
Hutchinsan <vgbmggjj36@gmail.com>
2025-09-16 19:34:36 -0700

    changed

--- js/application.js
+++
@@ -1,8 +0,0 @@
-animationDelay = 100;
-minSearchTime = 100;
-minEnemyTime = 50;
-
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
-});
